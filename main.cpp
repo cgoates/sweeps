740e7c0fb38d07890eafdb4add890188a5bc3d17
Caleb <calebgoates@gmail.com>
2024-01-19 12:46:07 -0700

    Trace from the centroid of every triangle in the source surface

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 #include <VTKOutput.hpp>
 #include <Logging.hpp>
 
-cgogn::CMap3::Face nthFaceWithVertsInSet( const cgogn::CMap3& map, const std::set<VertexId>& set, const size_t n )
+void foreachFaceWithVertsInSet( const cgogn::CMap3& map,
+                                const std::set<VertexId>& set,
+                                const std::function<bool( const cgogn::CMap3::Face&, const size_t n )>& callback )
 {
     cgogn::CMap3::Face out;
     const auto contains = [&]( const VertexId& vid ) {
@@ -18,10 +20,20 @@ cgogn::CMap3::Face nthFaceWithVertsInSet( const cgogn::CMap3& map, const std::se
         const VertexId vid1 = index_of( map, cgogn::CMap3::Vertex( d ) );
         const VertexId vid2 = index_of( map, cgogn::CMap3::Vertex( cgogn::phi1( map, d ) ) );
         const VertexId vid3 = index_of( map, cgogn::CMap3::Vertex( cgogn::phi_1( map, d ) ) );
-        if( contains( vid1 ) and contains( vid2 ) and contains( vid3 ) ) i++;
-        return i <= n;
+        if( contains( vid1 ) and contains( vid2 ) and contains( vid3 ) ) return callback( f, i++ );
+        return true;
     } );
-    return out;
+}
+
+
+void append( SimplicialComplex& append_to, const SimplicialComplex& to_append )
+{
+    const size_t offset = append_to.points.size();
+    append_to.points.insert( append_to.points.end(), to_append.points.begin(), to_append.points.end() );
+    std::transform( to_append.simplices.cbegin(), to_append.simplices.cend(), std::back_inserter( append_to.simplices ), [&]( const Simplex& s ) {
+        // FIXME: NOT GENERAL!!!
+        return Simplex( s.vertex( 0 ).id() + offset, s.vertex( 1 ).id() + offset );
+    } );
 }
 
 int main()
@@ -35,20 +47,35 @@ int main()
     const Eigen::VectorXd ans = solveLaplaceSparse( map, sweep_input.zero_bcs, sweep_input.one_bcs, normals );
 
     const Eigen::MatrixX3d grad = gradients( map, ans, normals );
-    cgogn::CMap3::Face start_face = nthFaceWithVertsInSet( map, sweep_input.zero_bcs, 10 );
-    const SimplicialComplex field_line = traceField( map, start_face, centroid( map, start_face ), grad, normals );
-    std::cout << "field_line: " << field_line.points << std::endl << std::endl;
 
     io::VTKOutputObject output( sweep_input.mesh );
     output.addVertexField( "laplace", ans );
     output.addCellField( "gradients", grad );
 
     // std::cout << ans.transpose() << std::endl;
-    io::outputSimplicialFieldToVTK( output, "test2.vtu" );
-    io::outputSimplicialFieldToVTK( map, ans, "test.vtu" );
+    io::outputSimplicialFieldToVTK( output, "test.vtu" );
 
-    io::VTKOutputObject line_output( field_line );
-    io::outputSimplicialFieldToVTK( line_output, "line.vtu" );
+    SimplicialComplex all_lines;
+
+    foreachFaceWithVertsInSet( map, sweep_input.zero_bcs, [&]( const cgogn::CMap3::Face& start_face, const size_t i ){
+        const SimplicialComplex field_line = traceField( map, start_face, centroid( map, start_face ), grad, normals );
+        // std::cout << "field_line: " << field_line.points << std::endl << std::endl;
+        if( field_line.simplices.size() < 40 )std::cout << "Line: " << i << " Length: " << field_line.simplices.size() << std::endl;
+        append( all_lines, field_line );
+        return true;
+    } );
+
+    io::VTKOutputObject line_output( all_lines );
+    io::outputSimplicialFieldToVTK( line_output, "line_full.vtu" );
+
+    foreachFaceWithVertsInSet( map, sweep_input.zero_bcs, [&]( const cgogn::CMap3::Face& start_face, const size_t i ){
+        if( i == 184 )
+        {
+            const SimplicialComplex field_line = traceField( map, start_face, centroid( map, start_face ), grad, normals, true );
+            // std::cout << "field_line: " << field_line.points << std::endl << std::endl;
+        }
+        return true;
+    } );
 
     std::cout << "dimension: " << (int)map.dimension << std::endl;
     std::cout << "Simplicial? " << cgogn::is_simplicial( map ) << std::endl;
