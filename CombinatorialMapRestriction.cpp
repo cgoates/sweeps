7087dc6bfc4ec0b8cc17d62577a7b6c063920cf0
Caleb Goates <calebgoates@gmail.com>
2024-03-09 10:43:33 -0700

    add: Restriction of a combinatorial map

---
+++ CombinatorialMapRestriction.cpp
@@ -0,0 +1,63 @@
+#include <CombinatorialMapRestriction.hpp>
+#include <CombinatorialMapMethods.hpp>
+
+using namespace topology;
+
+CombinatorialMapRestriction::CombinatorialMapRestriction( const CombinatorialMap& map,
+                                                          const std::function<bool( const Cell& )>& restriction_func )
+    : mUnrestrictedMap( map ), mIncludedDarts( map )
+{
+    topology::iterateCellsWhile( map, map.dim(), [&]( const Cell& elem ) {
+        if( restriction_func( elem ) )
+        {
+            iterateDartsOfCell( map, elem, [&]( const Dart& d ) {
+                mIncludedDarts.mark( d );
+                return true;
+            } );
+        }
+        return true;
+    } );
+}
+
+std::optional<Dart> CombinatorialMapRestriction::phi( const int i, const Dart& d ) const
+{
+    const auto candidate = topology::phi( mUnrestrictedMap, i, d );
+    if( std::abs( i ) != (int)dim() or not candidate.has_value() or mIncludedDarts.isMarked( candidate.value() ) )
+    {
+        return candidate;
+    }
+    return std::nullopt;
+}
+
+bool CombinatorialMapRestriction::iterateDartsWhile( const std::function<bool( const Dart& )>& callback ) const
+{
+    return topology::iterateDartsWhile( mUnrestrictedMap, [&]( const Dart& d ) {
+        if( mIncludedDarts.isMarked( d ) )
+        {
+            if( not callback( d ) ) return false;
+        }
+        return true;
+    } );
+}
+
+bool CombinatorialMapRestriction::iterateCellsWhile( const uint cell_dim, const std::function<bool( const Cell& )>& callback ) const
+{
+    return topology::iterateCellsWhile( mUnrestrictedMap, cell_dim, [&]( const Cell& c ) {
+        if( mIncludedDarts.isMarked( c.dart() ) )
+        {
+            if( not callback( c ) ) return false;
+        }
+        else
+        {
+            const bool continue_iterating = iterateDartsOfCell( mUnrestrictedMap, c, [&]( const Dart& d ) {
+                if( mIncludedDarts.isMarked( d ) )
+                {
+                    if( not callback( Cell( d, cell_dim ) ) ) return false;
+                }
+                return true;
+            } );
+            if( not continue_iterating ) return false;
+        }
+        return true;
+    } );
+}
---
+++ CombinatorialMapRestriction.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <CombinatorialMap.hpp>
+#include <GlobalDartMarker.hpp>
+
+namespace topology
+{
+    class CombinatorialMapRestriction : public CombinatorialMap
+    {
+        public:
+        /// @param map The map to restrict
+        /// @param restriction_func A restriction function called on each element. If true, that element is included.
+        CombinatorialMapRestriction( const CombinatorialMap& map,
+                                     const std::function<bool( const Cell& )>& restriction_func );
+
+        virtual std::optional<Dart> phi( const int i, const Dart& d ) const override;
+        virtual Dart::IndexType maxDartId() const override { return mUnrestrictedMap.maxDartId(); }
+        virtual uint dim() const override { return mUnrestrictedMap.dim(); }
+        virtual bool iterateDartsWhile( const std::function<bool( const Dart& )>& callback ) const override;
+        virtual bool iterateCellsWhile( const uint cell_dim, const std::function<bool( const Cell& )>& callback ) const override;
+
+        private:
+        const CombinatorialMap& mUnrestrictedMap;
+        GlobalDartMarker mIncludedDarts;
+    };
+}
