6ff40fe90f6adb7d1690a995879056c23d884a7c
Caleb Goates <calebgoates@gmail.com>
2024-04-03 07:25:55 -0600

    test: Tracing on the macaroni

--- test/tracing_tests.cpp
+++ test/tracing_tests.cpp
@@ -3,6 +3,13 @@
 #include <Logging.hpp>
 #include <SimplexUtilities.hpp>
 #include <CombinatorialMap.hpp>
+#include <TetMeshCombinatorialMap.hpp>
+#include <CombinatorialMapBoundary.hpp>
+#include <CombinatorialMapRestriction.hpp>
+#include <CombinatorialMapMethods.hpp>
+#include <GlobalCellMarker.hpp>
+#include <Laplace.hpp>
+#include <AbaqusInput.hpp>
 
 namespace topology
 {
@@ -147,4 +154,100 @@ TEST_CASE( "Test gradient tracing in triangle", "[single-file]" )
     }
 
     // std::cout << "Intersection: {" << intersection.value().first << ", " << intersection.value().second << std::endl;
+}
+
+TEST_CASE( "Tracing from all the cells in the macaroni", "[slow]")
+{
+    const SweepInput sweep_input =
+        io::loadINPFile( SRC_HOME "/test/data/macaroni.inp", "Surface3", "Surface4" );
+
+    const topology::TetMeshCombinatorialMap map( sweep_input.mesh );
+    const std::vector<Normal> normals = faceNormals( map );
+    std::cout << "Solving laplace\n";
+    const Eigen::VectorXd ans =
+        solveLaplaceSparse( map, sweep_input.zero_bcs, sweep_input.one_bcs, normals );
+
+    const topology::CombinatorialMapBoundary bdry( map );
+
+    const auto keep_face_sides = [&]( const topology::Face& f ) {
+        return ( not sweep_input.zero_bcs.at( bdry.vertexId( topology::Vertex( f.dart() ) ).id() ) or
+                    not sweep_input.zero_bcs.at(
+                        bdry.vertexId( topology::Vertex( phi( bdry, 1, f.dart() ).value() ) ).id() ) or
+                    not sweep_input.zero_bcs.at(
+                        bdry.vertexId( topology::Vertex( phi( bdry, -1, f.dart() ).value() ) ).id() ) ) and
+                ( not sweep_input.one_bcs.at( bdry.vertexId( topology::Vertex( f.dart() ) ).id() ) or
+                    not sweep_input.one_bcs.at(
+                        bdry.vertexId( topology::Vertex( phi( bdry, 1, f.dart() ).value() ) ).id() ) or
+                    not sweep_input.one_bcs.at(
+                        bdry.vertexId( topology::Vertex( phi( bdry, -1, f.dart() ).value() ) ).id() ) );
+    };
+
+    const auto keep_face_base = [&]( const topology::Face& f ) {
+        return sweep_input.zero_bcs.at( bdry.vertexId( topology::Vertex( f.dart() ) ).id() ) and
+                sweep_input.zero_bcs.at( bdry.vertexId( topology::Vertex( phi( bdry, 1, f.dart() ).value() ) ).id() ) and
+                sweep_input.zero_bcs.at( bdry.vertexId( topology::Vertex( phi( bdry, -1, f.dart() ).value() ) ).id() );
+    };
+
+    const auto keep_face_target = [&]( const topology::Face& f ) {
+        return sweep_input.one_bcs.at( bdry.vertexId( topology::Vertex( f.dart() ) ).id() ) and
+                sweep_input.one_bcs.at(
+                    bdry.vertexId( topology::Vertex( phi( bdry, 1, f.dart() ).value() ) ).id() ) and
+                sweep_input.one_bcs.at(
+                    bdry.vertexId( topology::Vertex( phi( bdry, -1, f.dart() ).value() ) ).id() );
+    };
+
+    const topology::CombinatorialMapRestriction sides( bdry, keep_face_sides );
+    const topology::CombinatorialMapRestriction base( bdry, keep_face_base );
+    const topology::CombinatorialMapRestriction target( bdry, keep_face_target );
+
+    const Eigen::Matrix3Xd grad = gradientsWithBoundaryCorrection( map, sides, ans, normals );
+
+    const auto vertex_positions = [&sweep_input]( const topology::CombinatorialMap& map ){
+        return [&sweep_input, &map]( const topology::Vertex& v ) -> const Eigen::Vector3d& {
+            return sweep_input.mesh.points.at( map.vertexId( v ).id() );
+        };
+    };
+
+    iterateCellsWhile( map, 2, [&]( const topology::Face& start_face ) {
+        if( onBoundary( map, start_face.dart() ) ) return true;
+        CHECK_NOTHROW( traceField( map, start_face, centroid( map, start_face ), grad, normals ) );
+        return true;
+    } );
+    iterateCellsWhile( base, 2, [&]( const topology::Face& start_face ) {
+        CHECK_NOTHROW( traceField( map, start_face, centroid( map, start_face ), grad, normals ) );
+        return true;
+    } );
+
+    iterateCellsWhile( sides, 1, [&]( const topology::Edge& start_edge ) {
+        const auto maybe_phi2 = phi( bdry, 2, start_edge.dart() );
+        if( maybe_phi2.has_value() and keep_face_target( maybe_phi2.value() ) ) return true;
+        CHECK_NOTHROW( traceBoundaryField( sides, start_edge, 0.5, ans, vertex_positions( sides ), false ) );
+        return true;
+    } );
+
+    const auto bdry_positions = vertex_positions( bdry );
+    topology::GlobalCellMarker traced_vertices( map, 0 );
+
+    iterateCellsWhile( base, 0, [&]( const topology::Vertex& v ) {
+        traced_vertices.mark( map, bdry.toUnderlyingCell( v ) );
+        return true;
+    } );
+
+    // Tracing reverse as a parameterization test
+    const auto reverse_ans = -1 * ans;
+    const auto reverse_grad = -1 * grad;
+
+    iterateCellsWhile( sides, 0, [&]( const topology::Vertex& v ) {
+        if( traced_vertices.isMarked( bdry.toUnderlyingCell( v ) ) ) return true;
+        traced_vertices.mark( map, bdry.toUnderlyingCell( v ) );
+        CHECK_NOTHROW( traceBoundaryField( sides, v, 1.0, reverse_ans, bdry_positions, false ) );
+        return true;
+    } );
+
+    const auto pos = vertex_positions( map );
+    iterateCellsWhile( map, 0, [&]( const topology::Vertex& v ) {
+        if( traced_vertices.isMarked( v ) ) return true;
+        CHECK_NOTHROW( traceField( map, v, pos( v ), reverse_grad, normals, false ) );
+        return true;
+    } );
 }
