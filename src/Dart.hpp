ddaf824e65b29d54215c7eb34eeae613518dbd69
Caleb Goates <calebgoates@gmail.com>
2024-04-15 13:06:30 -0600

    add: != comparator for Darts

--- src/Dart.hpp
+++ src/Dart.hpp
@@ -19,6 +19,8 @@ namespace topology
 
         bool operator==( const Dart& o ) const { return id() == o.id(); }
 
+        bool operator!=( const Dart& o ) const { return id() != o.id(); }
+
         private:
         IndexType mIndex;
     };
