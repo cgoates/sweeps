18ac03616e1dc0b34e2673251a9ce78ac0cff052
Caleb Goates <calebgoates@gmail.com>
2024-05-08 11:32:49 -0600

    test: Test the Tutte embedding on a simple problem

--- test/laplace_tests.cpp
+++ test/laplace_tests.cpp
@@ -4,7 +4,9 @@
 #include <Laplace.hpp>
 #include <Logging.hpp>
 #include <TetMeshCombinatorialMap.hpp>
+#include <TriMeshCombinatorialMap.hpp>
 #include <SimplicialComplexTestCases.hpp>
+#include <CombinatorialMapMethods.hpp>
 
 TEST_CASE( "Laplace patch test", "" )
 {
@@ -16,4 +18,47 @@ TEST_CASE( "Laplace patch test", "" )
     const Eigen::VectorXd sol = sweepEmbedding( map, sweep_input.zero_bcs, sweep_input.one_bcs, normals );
 
     REQUIRE( equals( sol( 8 ), sweep_input.mesh.points.back()( 2 ), sweep_input.mesh.points.back()( 2 ) * 1e-15 ) );
+}
+
+TEST_CASE( "Tutte embedding patch test" )
+{
+    // A perterbation of this:
+    // *-------*
+    // | \   / |
+    // |  \ /  |
+    // |   *   |
+    // |  / \  |
+    // | /   \ |
+    // *-------*
+    SimplicialComplex mesh{
+        { { 0, 1, 4 }, { 0, 4, 2 }, { 4, 1, 3 }, { 4, 3, 2 } },
+        { { 0.0, 0.0, 2.0 }, { 1.0, 0.0, 1.2 }, { 0.0, 1.0, 0.7 }, { 1.5, 1.2, 0.2 }, { 0.7, 0.6, 0.0 } } };
+
+    const topology::TriMeshCombinatorialMap map( mesh );
+
+    std::vector<Eigen::Vector2d> constraints{ { 0.0, 0.0 }, { 1.0, 0.0 }, { 0.0, 1.0 }, { 1.0, 1.0 } };
+
+    const auto vids = indexingOrError( map, 0 );
+
+    const auto constraints_func = [&]( const topology::Vertex& v ) -> std::optional<Eigen::Vector2d> {
+        if( not boundaryAdjacent( map, v ) ) return std::nullopt;
+        return constraints.at( vids( v ) );
+    };
+
+    const auto vert_positions = [&]( const topology::Vertex& v ){
+        return mesh.points.at( vids( v ) );
+    };
+
+    const Eigen::MatrixX2d tutte = tutteEmbedding( map, vert_positions, constraints_func, false );
+
+    const Eigen::MatrixX2d expected = ( Eigen::MatrixX2d( 5, 2 ) << 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.5, 0.5 ).finished();
+
+
+    std::cout << tutte << std::endl;
+    REQUIRE( tutte.rows() == 5 );
+    for( Eigen::Index i = 0; i < 5; i++ )
+    {
+        CHECK( tutte( i, 0 ) == expected( i, 0 ) );
+        CHECK( tutte( i, 1 ) == expected( i, 1 ) );
+    }
 }
