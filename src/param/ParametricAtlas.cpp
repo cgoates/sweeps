04cf0a94b6b26368faeb524af9fa34cf1bc61b1a
Caleb Goates <calebgoates@gmail.com>
2024-05-16 16:21:35 -0600

    add: parentDomainBoundary to get boundary is zero vec for k-cell

--- src/param/ParametricAtlas.cpp
+++ src/param/ParametricAtlas.cpp
@@ -1,1 +1,20 @@
 #include <ParametricAtlas.hpp>
+#include <CombinatorialMapMethods.hpp>
+
+namespace param
+{
+    BaryCoordIsZeroVec parentDomainBoundary( const ParametricAtlas& atlas, const topology::Cell& cell )
+    {
+        if( cell.dim() == 0 ) return atlas.parentPoint( cell ).mBaryCoordIsZero;
+        else
+        {
+            BaryCoordIsZeroVec out( numTotalCoordinates( atlas.parentDomain( topology::Cell( cell.dart(), atlas.cmap().dim() ) ) ), true );
+            iterateAdjacentCellsOfRestrictedCell( atlas.cmap(), cell, atlas.cmap().dim(), 0, [&]( const topology::Vertex& v ) {
+                const ParentPoint ppt = atlas.parentPoint( v );
+                out = join( out, ppt.mBaryCoordIsZero );
+                return true;
+            } );
+            return out;
+        }
+    }
+}
--- src/param/ParametricAtlas.hpp
+++ src/param/ParametricAtlas.hpp
@@ -6,8 +6,11 @@ namespace param
 {
     class ParametricAtlas
     {
+        public:
         virtual const topology::CombinatorialMap& cmap() const = 0;
         virtual const ParentDomain& parentDomain( const topology::Cell& c ) const = 0;
         virtual ParentPoint parentPoint( const topology::Vertex& v ) const = 0;
     };
+
+    BaryCoordIsZeroVec parentDomainBoundary( const ParametricAtlas& atlas, const topology::Cell& cell );
 }
