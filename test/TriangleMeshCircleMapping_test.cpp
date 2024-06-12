529a1c90bd925b7c8d8237bad79fd8d312434a53
Caleb Goates <calebgoates@gmail.com>
2024-06-12 11:40:09 -0600

    test: Failing test of triangle circle mapping with two boundary edges on a face

--- test/TriangleMeshCircleMapping_test.cpp
+++ test/TriangleMeshCircleMapping_test.cpp
@@ -211,3 +211,112 @@ TEST_CASE( "Mesh wide circle map inverse test" )
     test_inverse( { 0.25,  0.25}, topology::Face( topology::Dart( 9 ) ), {0.176776695, 0.176776695} );
     test_no_inverse( {0.9, 0.5} );
 }
+
+TEST_CASE( "Simple circle evaluations two boundary edges" )
+{
+    // This but fit boundary edges to a circle:
+    //   *
+    //  / \.
+    // *---*
+    //  \ /
+    //   *
+    const SimplicialComplex mesh{
+        { { 0, 1, 3 }, { 0, 3, 2 } },
+        { { -1.0, 0.0, 0.0 }, { 0.0, -1.0, 0.0 }, { 0.0, 1.0, 0.0 }, { 1.0, 0.0, 0.0 } } };
+
+    const topology::TriMeshCombinatorialMap cmap( mesh );
+    const auto vert_id = indexingOrError( cmap, 0 );
+    const param::TriangleParametricAtlas param( cmap );
+    const auto vert_positions = [&]( const topology::Vertex& v ) {
+        return mesh.points.at( vert_id( v ) ).head<2>();
+    };
+    const mapping::TriangleMeshCircleMapping geom_mapping( param, vert_positions );
+
+    const param::ParentDomain pd = param::simplexDomain( 2 );
+    iterateCellsWhile( cmap, 2, [&]( const topology::Face& f ) {
+        iterateAdjacentCells( cmap, f, 1, [&]( const topology::Edge& e ) {
+            const param::ParentPoint ppt = pointOnBoundary( pd, parentDomainBoundary( param, e ) );
+            const Eigen::Vector2d pos = geom_mapping.evaluate( f, ppt );
+            if( boundaryAdjacent( cmap, e ) )
+                CHECK( util::equals( pos.norm(), 1.0, 1e-12 ) );
+            else
+                CHECK( util::equals( pos.norm(), 0.0, 1e-12 ) );
+            return true;
+        } );
+
+        iterateAdjacentCells( cmap, f, 0, [&]( const topology::Vertex& v ) {
+            const param::ParentPoint ppt = param.parentPoint( v );
+            const Eigen::Vector2d pos = geom_mapping.evaluate( f, ppt );
+            const Eigen::Vector2d expected = vert_positions( v );
+            CHECK( util::equals( pos, expected, 1e-12 ) );
+            return true;
+        } );
+
+        const param::ParentPoint ppt = pointOnBoundary( pd, parentDomainBoundary( param, f ) );
+        const Eigen::Vector2d pos = geom_mapping.evaluate( f, ppt );
+        CHECK( util::equals( pos.norm(), 1.0/3.0, 1e-12 ) );
+        return true;
+    } );
+}
+
+TEST_CASE( "Circle mapping and inverse round trip test with two boundary edge tris" )
+{
+    // This but fit boundary edges to a circle:
+    //   *
+    //  / \.
+    // *---*
+    //  \ /
+    //   *
+    const SimplicialComplex mesh{
+        { { 0, 1, 3 }, { 0, 3, 2 } },
+        { { -1.0, 0.0, 0.0 }, { 0.0, -1.0, 0.0 }, { 0.0, 1.0, 0.0 }, { 1.0, 0.0, 0.0 } } };
+
+    const topology::TriMeshCombinatorialMap cmap( mesh );
+    const auto vert_id = indexingOrError( cmap, 0 );
+    const param::TriangleParametricAtlas param( cmap );
+    const auto vert_positions = [&]( const topology::Vertex& v ) {
+        return mesh.points.at( vert_id( v ) ).head<2>();
+    };
+    const mapping::TriangleMeshCircleMapping geom_mapping( param, vert_positions );
+
+    const param::ParentDomain pd = param::simplexDomain( 2 );
+    iterateCellsWhile( cmap, 2, [&]( const topology::Face& f ) {
+        iterateAdjacentCells( cmap, f, 1, [&]( const topology::Edge& e ) {
+            const param::ParentPoint ppt = pointOnBoundary( pd, parentDomainBoundary( param, e ) );
+            const Eigen::Vector2d pos = geom_mapping.evaluate( f, ppt );
+            const auto ppt_rt = geom_mapping.maybeInverse( f, pos );
+            CHECK( ppt_rt.has_value() );
+            if( ppt_rt.has_value() )
+            {
+                CHECK( ppt_rt.value().mBaryCoordIsZero == ppt.mBaryCoordIsZero );
+                CHECK( util::equals( ppt_rt->mPoint, ppt.mPoint, 1e-10 ) );
+            }
+            return true;
+        } );
+
+        iterateAdjacentCells( cmap, f, 0, [&]( const topology::Vertex& v ) {
+            const param::ParentPoint ppt = param.parentPoint( v );
+            const Eigen::Vector2d pos = geom_mapping.evaluate( f, ppt );
+            const auto ppt_rt = geom_mapping.maybeInverse( f, pos );
+            CHECK( ppt_rt.has_value() );
+            if( ppt_rt.has_value() )
+            {
+                CHECK( ppt_rt.value().mBaryCoordIsZero == ppt.mBaryCoordIsZero );
+                CHECK( util::equals( ppt_rt->mPoint, ppt.mPoint, 1e-10 ) );
+
+            }
+            return true;
+        } );
+
+        const param::ParentPoint ppt = pointOnBoundary( pd, {false, false, false} );
+        const Eigen::Vector2d pos = geom_mapping.evaluate( f, ppt );
+        const auto ppt_rt = geom_mapping.maybeInverse( f, pos );
+        CHECK( ppt_rt.has_value() );
+        if( ppt_rt.has_value() )
+        {
+            CHECK( ppt_rt.value().mBaryCoordIsZero == ppt.mBaryCoordIsZero );
+            CHECK( util::equals( ppt_rt->mPoint, ppt.mPoint, 1e-10 ) );
+        }
+        return true;
+    } );
+}
