94a51f63efaf26c47138ddf0a317c608188fd005
Caleb Goates <calebgoates@gmail.com>
2024-06-12 09:23:47 -0600

    fix: triangle circle mapping in case where a tri had two boundary edges

--- src/mapping/TriangleMeshCircleMapping.cpp
+++ src/mapping/TriangleMeshCircleMapping.cpp
@@ -20,17 +20,19 @@ namespace mapping
         } );
     }
 
-    std::optional<topology::Edge> maybeBoundaryEdge( const topology::CombinatorialMap& cmap, const topology::Face& f )
+    SmallVector<topology::Edge, 3> maybeBoundaryEdges( const topology::CombinatorialMap& cmap, const topology::Face& f )
     {
-        std::optional<topology::Edge> out;
-        iterateDartsOfCell( cmap, f, [&]( const topology::Dart& d ){
-            if( not phi( cmap, cmap.dim(), d ).has_value() )
-            {
-                out.emplace( d );
-                return false;
-            }
-            return true;
-        } );
+        SmallVector<topology::Edge, 3> out;
+        topology::Dart curr_d = f.dart();
+        do
+        {
+            if( onBoundary( cmap, curr_d ) ) out.push_back( curr_d );
+
+            const auto phi1 = phi( cmap, 1, curr_d );
+            if( phi1.has_value() ) curr_d = phi1.value();
+            else break;
+        } while( curr_d != f.dart() );
+
         return out;
     }
 
@@ -41,42 +43,108 @@ namespace mapping
                               std::find( v_pt.mBaryCoordIsZero.begin(), v_pt.mBaryCoordIsZero.end(), false ) );
     }
 
+    Eigen::Vector2d triangleWithArcEdgeEval( const std::array<double, 3>& bary, const std::array<double, 2>& thetas, const Eigen::Vector2d& v2_position )
+    {
+        const double& theta0 = thetas[0];
+        const double theta1 = [&]() {
+            double temp = thetas[1];
+            while( temp < theta0 ) temp += 2 * std::numbers::pi;
+            return temp;
+        }();
+        // 1. Get the theta value
+        const double theta = ( theta0 * bary[0] + theta1 * bary[1] ) / ( bary[0] + bary[1] );
+
+        if( std::isinf( theta ) or std::isnan( theta ) ) throw std::runtime_error( "Bad parent point provided to TriangleMeshCircleMapping::evaluate" );
+
+        // 2. Evaluate the arc at that value
+        const Eigen::Vector2d boundary_point( cos( theta ), sin( theta ) );
+
+        // 3. Interpolate between the arc and the third point
+        return ( bary[0] + bary[1] ) * boundary_point + bary[2] * v2_position;
+    }
+
     Eigen::VectorXd TriangleMeshCircleMapping::evaluate( const topology::Cell& c, const param::ParentPoint& pt ) const
     {
         if( c.dim() != 2 ) throw std::runtime_error( "Bad cell dimension for TriangleMeshCircleMapping::evaluate" );
 
-        const std::optional<topology::Edge> boundary_edge = maybeBoundaryEdge( mAtlas.cmap(), c );
-        if( boundary_edge.has_value() )
+        const SmallVector<topology::Edge, 3> boundary_edges = maybeBoundaryEdges( mAtlas.cmap(), c );
+        switch( boundary_edges.size() )
         {
-            const Vector6dMax expanded_coords = expandedCoordinates( pt );
-            const auto vert_ids = indexingOrError( mAtlas.cmap(), 0 );
-            const topology::Vertex v0( boundary_edge.value().dart() );
-            const topology::Vertex v1( phi( mAtlas.cmap(), 1, boundary_edge.value().dart() ).value() );
-            const topology::Vertex v2( phi( mAtlas.cmap(), -1, boundary_edge.value().dart() ).value() );
-            const double& bary0 = expanded_coords( vertex_ii( mAtlas, v0 ) );
-            const double& bary1 = expanded_coords( vertex_ii( mAtlas, v1 ) );
-            const double& bary2 = expanded_coords( vertex_ii( mAtlas, v2 ) );
-            const double theta0 = mBoundaryAngles.at( vert_ids( v0 ) );
-            const double theta1 = [&]() {
-                double temp = mBoundaryAngles.at( vert_ids( v1 ) );
-                while( temp < theta0 ) temp += 2 * std::numbers::pi;
-                return temp;
-            }();
-            // 1. Get the theta value
-            const double theta = ( theta0 * bary0 + theta1 * bary1 ) / ( bary0 + bary1 );
+            case 0: return mTriMapping.evaluate( c, pt );
+            case 1:
+            {
+                const topology::Edge& boundary_edge = boundary_edges.at( 0 );
+                const Vector6dMax expanded_coords = expandedCoordinates( pt );
+                const auto vert_ids = indexingOrError( mAtlas.cmap(), 0 );
+                const std::array<topology::Vertex, 3> vertices{
+                    topology::Vertex( boundary_edge.dart() ),
+                    topology::Vertex( phi( mAtlas.cmap(), 1, boundary_edge.dart() ).value() ),
+                    topology::Vertex( phi( mAtlas.cmap(), -1, boundary_edge.dart() ).value() )
+                };
+                const std::array<size_t, 3> corner_ids{
+                    vertex_ii( mAtlas, vertices[0] ),
+                    vertex_ii( mAtlas, vertices[1] ),
+                    vertex_ii( mAtlas, vertices[2] )
+                };
 
-            // Check for bary0 + bary1 = 0 without toleranced compares.  Bad idea?
-            if( std::isinf( theta ) or std::isnan( theta ) ) return mTriMapping.vertPositions()( v2 );
+                const std::array<double, 3> bary{
+                    expanded_coords( vertex_ii( mAtlas, vertices[0] ) ),
+                    expanded_coords( vertex_ii( mAtlas, vertices[1] ) ),
+                    expanded_coords( vertex_ii( mAtlas, vertices[2] ) )
+                };
+                const double theta0 = mBoundaryAngles.at( vert_ids( vertices[0] ) );
+                const double theta1 = mBoundaryAngles.at( vert_ids( vertices[1] ) );
 
-            // 2. Evaluate the arc at that value
-            const Eigen::Vector2d boundary_point( cos( theta ), sin( theta ) );
+                if( pt.mBaryCoordIsZero.at( corner_ids[0] ) and pt.mBaryCoordIsZero.at( corner_ids[1] ) )
+                    return mTriMapping.vertPositions()( vertices[2] );
 
-            // 3. Interpolate between the arc and the third point
-            return ( bary0 + bary1 ) * boundary_point + bary2 * mTriMapping.vertPositions()( v2 );
-        }
-        else
-        {
-            return mTriMapping.evaluate( c, pt );
+                return triangleWithArcEdgeEval( bary, {theta0, theta1},  mTriMapping.vertPositions()( vertices[2] ) );
+            }
+            case 2:
+            {
+                const Vector6dMax expanded_coords = expandedCoordinates( pt );
+                const auto vert_ids = indexingOrError( mAtlas.cmap(), 0 );
+                const std::array<topology::Vertex, 3> vertices{
+                    topology::Vertex( boundary_edges.at( 0 ).dart() ),
+                    topology::Vertex( boundary_edges.at( 1 ).dart() ),
+                    topology::Vertex( phi( mAtlas.cmap(), 1, boundary_edges.at( 1 ).dart() ).value() )
+                };
+                const std::array<size_t, 3> corner_ids{
+                    vertex_ii( mAtlas, vertices[0] ),
+                    vertex_ii( mAtlas, vertices[1] ),
+                    vertex_ii( mAtlas, vertices[2] )
+                };
+
+                const std::array<double, 3> bary{
+                    expanded_coords( vertex_ii( mAtlas, vertices[0] ) ),
+                    expanded_coords( vertex_ii( mAtlas, vertices[1] ) ),
+                    expanded_coords( vertex_ii( mAtlas, vertices[2] ) )
+                };
+
+                if( pt.mBaryCoordIsZero.at( corner_ids[1] ) )
+                    return bary[0] * mTriMapping.vertPositions()( vertices[0] ) +
+                           bary[2] * mTriMapping.vertPositions()( vertices[2] );
+
+                const Eigen::Vector2d edge_midpoint =
+                    0.5 * mTriMapping.vertPositions()( vertices[0] ) + 0.5 * mTriMapping.vertPositions()( vertices[2] );
+
+                if( expanded_coords( corner_ids[ 0 ] ) > expanded_coords( corner_ids[ 2 ] ) )
+                {
+                    // right side triangle
+                    const double theta0 = mBoundaryAngles.at( vert_ids( vertices[0] ) );
+                    const double theta1 = mBoundaryAngles.at( vert_ids( vertices[1] ) );
+                    return triangleWithArcEdgeEval( { bary[ 0 ] - bary[ 2 ], bary[ 1 ], 2 * bary[ 2 ] }, {theta0, theta1}, edge_midpoint );
+                }
+                else
+                {
+                    // left side triangle
+                    const double theta1 = mBoundaryAngles.at( vert_ids( vertices[1] ) );
+                    const double theta2 = mBoundaryAngles.at( vert_ids( vertices[2] ) );
+                    return triangleWithArcEdgeEval( { bary[ 1 ], bary[ 2 ] - bary[ 0 ], 2 * bary[ 0 ] }, {theta1, theta2}, edge_midpoint );
+                }
+            }
+            default:
+                throw std::runtime_error( "Face with three or more boundary edges cannot exist in a TriangleMeshCircleMap");
         }
     }
 
@@ -109,49 +177,109 @@ namespace mapping
         return std::abs( a - b ) < std::numbers::pi;
     }
 
+    std::optional<Eigen::Vector3d> invertTriangleWithArcEdgeEval( const Eigen::Vector2d& pt, const Eigen::Vector2d& non_bdry_point, const std::array<double, 2>& thetas )
+    {
+        const Eigen::Vector2d bdry_point = lineCircleIntersection( non_bdry_point, pt );
+        const double bdry_point_theta = atan2( bdry_point( 1 ), bdry_point( 0 ) );
+
+        if( not isBetweenAngles( thetas[ 0 ], thetas[ 1 ], bdry_point_theta ) ) return std::nullopt;
+
+        const auto [ lambda_non_bdry, lambda_bdry ] = [&](){
+            if( std::abs( bdry_point( 0 ) - non_bdry_point( 0 ) ) > std::abs( bdry_point( 1 ) - non_bdry_point( 1 ) ) )
+                return inverseLinear( non_bdry_point(0), bdry_point(0), pt(0) );
+            else
+                return inverseLinear( non_bdry_point(1), bdry_point(1), pt(1) );
+        }();
+
+        const auto [lambda_v0, lambda_v1] = inverseLinear(
+            util::normalizeAngle( thetas[ 0 ] - bdry_point_theta ), util::normalizeAngle( thetas[ 1 ] - bdry_point_theta ), 0.0 );
+
+        Eigen::Vector3d bary = Eigen::Vector3d::Zero();
+        bary( 0 ) = lambda_v0 * lambda_bdry;
+        bary( 1 ) = lambda_v1 * lambda_bdry;
+        bary( 2 ) = lambda_non_bdry;
+        return bary;
+    }
+
     std::optional<param::ParentPoint> TriangleMeshCircleMapping::maybeInverse( const topology::Face& f, const Eigen::Vector2d& pt ) const
     {
-        const std::optional<topology::Edge> boundary_edge = maybeBoundaryEdge( mAtlas.cmap(), f );
-        if( boundary_edge.has_value() )
+        const SmallVector<topology::Edge, 3> boundary_edges = maybeBoundaryEdges( mAtlas.cmap(), f );
+        switch( boundary_edges.size() )
         {
-            const topology::Vertex v0( boundary_edge.value().dart() );
-            const topology::Vertex v1( phi( mAtlas.cmap(), 1, v0.dart() ).value() );
-            const topology::Vertex v2( phi( mAtlas.cmap(), 1, v1.dart() ).value() );
-            const Eigen::Vector2d non_bdry_point = mTriMapping.vertPositions()( v2 );
+            case 0: return mTriMapping.maybeInverse( f, pt );
+            case 1:
+            {
+                const std::array<topology::Vertex, 3> vertices{
+                    topology::Vertex( boundary_edges.at( 0 ).dart() ),
+                    topology::Vertex( phi( mAtlas.cmap(), 1, boundary_edges.at( 0 ).dart() ).value() ),
+                    topology::Vertex( phi( mAtlas.cmap(), -1, boundary_edges.at( 0 ).dart() ).value() )
+                };
+                
+                const Eigen::Vector2d non_bdry_point = mTriMapping.vertPositions()( vertices[ 2 ] );
 
-            const Eigen::Vector2d one_bdry_vert_point = mTriMapping.vertPositions()( v0 );
-            if( ( pt - non_bdry_point ).norm() < 1e-10 * ( one_bdry_vert_point - non_bdry_point ).norm() ) return mAtlas.parentPoint( v2 );
+                const Eigen::Vector2d one_bdry_vert_point = mTriMapping.vertPositions()( vertices[ 0 ] );
+                if( ( pt - non_bdry_point ).norm() < 1e-10 * ( one_bdry_vert_point - non_bdry_point ).norm() ) return mAtlas.parentPoint( vertices[ 2 ] );
 
-            const auto vert_ids = indexingOrError( mAtlas.cmap(), 0 );
-            const double theta0 = mBoundaryAngles.at( vert_ids( v0 ) );
-            const double theta1 = mBoundaryAngles.at( vert_ids( v1 ) );
+                const auto vert_ids = indexingOrError( mAtlas.cmap(), 0 );
+                const double theta0 = mBoundaryAngles.at( vert_ids( vertices[ 0 ] ) );
+                const double theta1 = mBoundaryAngles.at( vert_ids( vertices[ 1 ] ) );
 
-            const Eigen::Vector2d bdry_point = lineCircleIntersection( non_bdry_point, pt );
-            const double bdry_point_theta = atan2( bdry_point( 1 ), bdry_point( 0 ) );
+                return invertTriangleWithArcEdgeEval( pt, non_bdry_point, { theta0, theta1 } )
+                    .transform( [&]( const Eigen::Vector3d& bary ) {
+                        Eigen::Vector3d reordered_bary;
+                        reordered_bary( vertex_ii( mAtlas, vertices[0] ) ) = bary( 0 );
+                        reordered_bary( vertex_ii( mAtlas, vertices[1] ) ) = bary( 1 );
+                        reordered_bary( vertex_ii( mAtlas, vertices[2] ) ) = bary( 2 );
+                        const param::ParentDomain pd = mAtlas.parentDomain( f );
+                        return compressCoordinates( pd, reordered_bary, 1e-5 );
+                    } );
+            }
+            case 2:
+            {
+                const std::array<topology::Vertex, 3> vertices{
+                    topology::Vertex( boundary_edges.at( 0 ).dart() ),
+                    topology::Vertex( boundary_edges.at( 1 ).dart() ),
+                    topology::Vertex( phi( mAtlas.cmap(), 1, boundary_edges.at( 1 ).dart() ).value() )
+                };
 
-            if( not isBetweenAngles( theta0, theta1, bdry_point_theta ) ) return std::nullopt;
+                const Eigen::Vector2d non_bdry_point =
+                    0.5 * mTriMapping.vertPositions()( vertices[0] ) + 0.5 * mTriMapping.vertPositions()( vertices[2] );
 
-            const auto [ lambda_non_bdry, lambda_bdry ] = [&](){
-                if( std::abs( bdry_point( 0 ) - non_bdry_point( 0 ) ) > std::abs( bdry_point( 1 ) - non_bdry_point( 1 ) ) )
-                    return inverseLinear( non_bdry_point(0), bdry_point(0), pt(0) );
-                else
-                    return inverseLinear( non_bdry_point(1), bdry_point(1), pt(1) );
-            }();
+                const Eigen::Vector2d one_bdry_vert_point = mTriMapping.vertPositions()( vertices[ 0 ] );
+                if( ( pt - non_bdry_point ).norm() < 1e-10 * ( one_bdry_vert_point - non_bdry_point ).norm() )
+                    return average( mAtlas.parentPoint( vertices[2] ), mAtlas.parentPoint( vertices[0] ) );
 
-            const auto [lambda_v0, lambda_v1] = inverseLinear(
-                util::normalizeAngle( theta0 - bdry_point_theta ), util::normalizeAngle( theta1 - bdry_point_theta ), 0.0 );
+                const auto vert_ids = indexingOrError( mAtlas.cmap(), 0 );
+                const double theta0 = mBoundaryAngles.at( vert_ids( vertices[ 0 ] ) );
+                const double theta1 = mBoundaryAngles.at( vert_ids( vertices[ 1 ] ) );
+                const double theta2 = mBoundaryAngles.at( vert_ids( vertices[ 2 ] ) );
 
-            Eigen::Vector3d bary = Eigen::Vector3d::Zero();
-            bary( vertex_ii( mAtlas, v0 ) ) = lambda_v0 * lambda_bdry;
-            bary( vertex_ii( mAtlas, v1 ) ) = lambda_v1 * lambda_bdry;
-            bary( vertex_ii( mAtlas, v2 ) ) = lambda_non_bdry;
-
-            const param::ParentDomain pd = mAtlas.parentDomain( f );
-            return compressCoordinates( pd, bary, 1e-5 );
-        }
-        else
-        {
-            return mTriMapping.maybeInverse( f, pt );
+                bool first_side = true;
+                return invertTriangleWithArcEdgeEval( pt, non_bdry_point, { theta0, theta1 } )
+                    .or_else( [&]() -> std::optional<Eigen::Vector3d> {
+                        first_side = false;
+                        return invertTriangleWithArcEdgeEval( pt, non_bdry_point, { theta1, theta2 } );
+                    } )
+                    .transform( [&]( const Eigen::Vector3d& bary ) {
+                        Eigen::Vector3d reordered_bary;
+                        if( first_side )
+                        {
+                            reordered_bary( vertex_ii( mAtlas, vertices[0] ) ) = bary( 0 ) + 0.5 * bary( 2 );
+                            reordered_bary( vertex_ii( mAtlas, vertices[1] ) ) = bary( 1 );
+                            reordered_bary( vertex_ii( mAtlas, vertices[2] ) ) = 0.5 * bary( 2 );
+                        }
+                        else
+                        {
+                            reordered_bary( vertex_ii( mAtlas, vertices[0] ) ) = 0.5 * bary( 0 );
+                            reordered_bary( vertex_ii( mAtlas, vertices[1] ) ) = bary( 1 );
+                            reordered_bary( vertex_ii( mAtlas, vertices[2] ) ) = bary( 2 ) + 0.5 * bary( 0 );
+                        }
+                        const param::ParentDomain pd = mAtlas.parentDomain( f );
+                        return compressCoordinates( pd, bary, 1e-5 );
+                    } );
+            }
+            default:
+                throw std::runtime_error( "Face with three or more boundary edges cannot exist in a TriangleMeshCircleMap");
         }
     }
 
