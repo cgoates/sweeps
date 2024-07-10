32933a5ca894a61c6d5481b1cf33bb9d99afaab6
Caleb Goates <calebgoates@gmail.com>
2024-07-10 14:26:15 -0600

    test: TPSplineSpace

--- test/SplineSpace1d_test.cpp
+++ test/SplineSpace1d_test.cpp
@@ -7,6 +7,7 @@
 #include <CombinatorialMapMethods.hpp>
 #include <Logging.hpp>
 #include <CommonUtils.hpp>
+#include <TPSplineSpace.hpp>
 
 using namespace basis;
 using namespace param;
@@ -27,6 +28,38 @@ TEST_CASE( "Extraction of cubic spline with various knot multiplicities" )
         CHECK( util::equals( op.colwise().sum(), Eigen::VectorXd::Ones( op.cols() ), 1e-12 ) );
         CHECK( ss.connectivity( e ).size() == degree + 1 );
         CHECK( op.rows() == degree + 1 );
+        CHECK( op.rows() == op.cols() );
+
+        Eigen::FullPivLU<Eigen::MatrixXd> lu( op );
+        CHECK( lu.rank() == op.rows() );
+        return true;
+    } );
+}
+
+TEST_CASE( "TP Spline space" )
+{
+    const KnotVector kv1( {0,0,0,0,1,2,2,2,2}, 1e-10 );
+    const KnotVector kv2( {0,0,0,1,2,2,2}, 1e-10 );
+    const size_t degree1 = 3;
+    const size_t degree2 = 2;
+
+    const CombinatorialMap1d cmap( 2 );
+    const ParametricAtlas1d param( cmap, parametricLengths( kv1 ) );
+    const BasisComplex1d bc1( param, degree1 );
+    const BasisComplex1d bc2( param, degree2 );
+    const GenericSplineSpace ss1 = knotVectorSplineSpace( bc1, kv1 );
+    const GenericSplineSpace ss2 = knotVectorSplineSpace( bc2, kv2 );
+
+    const TPCombinatorialMap cmap_2d( cmap, cmap );
+    const TPParametricAtlas param_2d( cmap_2d, param, param );
+    const TPBasisComplex bc_2d( param_2d, bc1, bc2 );
+    const TPSplineSpace ss_2d( bc_2d, ss1, ss2 );
+
+    iterateCellsWhile( cmap_2d, 2, [&]( const topology::Face& f ) {
+        const Eigen::MatrixXd op = ss_2d.extractionOperator( f );
+        CHECK( util::equals( op.colwise().sum(), Eigen::VectorXd::Ones( op.cols() ), 1e-12 ) );
+        CHECK( ss_2d.connectivity( f ).size() == ( degree1 + 1 ) * ( degree2 + 1 ) );
+        CHECK( op.rows() == ( degree1 + 1 ) * ( degree2 + 1 ) );
         CHECK( op.rows() == op.cols() );
 
         Eigen::FullPivLU<Eigen::MatrixXd> lu( op );
