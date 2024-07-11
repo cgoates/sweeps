e69ae82c1c91adee264464e31e9879e4e8943b1f
Caleb Goates <calebgoates@gmail.com>
2024-07-11 14:14:18 -0600

    add: Tensor product indices operations

---
+++ src/util/IndexOperations.cpp
@@ -0,0 +1,155 @@
+#include <IndexOperations.hpp>
+#include <numeric>
+
+namespace util
+{
+    size_t flatten( const IndexVec& tp_indices, const IndexVec& lengths )
+    {
+        size_t multiplier = 1;
+        size_t out = 0;
+        for( size_t i = 0; i < tp_indices.size(); i++ )
+        {
+            out += multiplier * tp_indices.at( i );
+            multiplier *= lengths.at( i );
+        }
+        return out;
+    }
+
+    IndexVec unflatten( const size_t index, const IndexVec& lengths )
+    {
+        IndexVec out;
+        size_t divisor = 1;
+        size_t mod_divisor = 1;
+        for( size_t i = 0; i < lengths.size(); i++ )
+        {
+            mod_divisor *= lengths.at( i );
+            out.push_back( ( index % mod_divisor ) / divisor );
+            divisor *= lengths.at( i );
+        }
+        return out;
+    }
+
+    void iterateTensorProduct( const IndexVec& lengths, const std::function<void( const IndexVec& )>& callback )
+    {
+        const size_t num_ids = std::reduce( lengths.begin(), lengths.end(), 1, std::multiplies<>() );
+        for( size_t id = 0; id < num_ids; id++ )
+            callback( unflatten( id, lengths ) );
+    }
+
+    void iterateVTKTPOrdering( const IndexVec& lengths, const std::function<void( const size_t& )>& callback )
+    {
+        // There is probably a more unified way to write this...
+        switch( lengths.size() )
+        {
+            case 1:
+                callback( 0 );
+                if( lengths.at( 0 ) > 1 ) callback( lengths.at( 0 ) - 1 );
+                for( size_t i = 1; i < lengths.at( 0 ) - 1; i++ )
+                    callback( i );
+                break;
+            case 2:
+            {
+                if( lengths.at( 0 ) < 2 or lengths.at( 1 ) < 2 )
+                    throw std::runtime_error( "Cannot iterate with lengths < 2" ); //FIXME
+
+                // Corners in CCW order
+                callback( flatten( { 0, 0 }, lengths ) );
+                callback( flatten( { lengths.at( 0 ) - 1, 0 }, lengths ) );
+                callback( flatten( { lengths.at( 0 ) - 1, lengths.at( 1 ) - 1 }, lengths ) );
+                callback( flatten( { 0, lengths.at( 1 ) - 1 }, lengths ) );
+
+                // Edges in CCW order, iterated in positive s or t direction
+                for( size_t i = 1; i < lengths.at( 0 ) - 1; i++ )
+                    callback( flatten( {i, 0}, lengths ) );
+                for( size_t i = 1; i < lengths.at( 1 ) - 1; i++ )
+                    callback( flatten( {lengths.at( 0 ) - 1, i}, lengths ) );
+                for( size_t i = 1; i < lengths.at( 0 ) - 1; i++ )
+                    callback( flatten( {i, lengths.at( 1 ) - 1}, lengths ) );
+                for( size_t i = 1; i < lengths.at( 1 ) - 1; i++ )
+                    callback( flatten( {0, i}, lengths ) );
+
+                // Interior in tensor product order
+                iterateTensorProduct( { lengths.at( 0 ) - 2, lengths.at( 1 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { interior_index.at( 0 ) + 1, interior_index.at( 1 ) + 1 }, lengths ) );
+                } );
+            }
+            break;
+            case 3:
+            {
+                if( lengths.at( 0 ) < 2 or lengths.at( 1 ) < 2 or lengths.at( 2 ) < 2 )
+                    throw std::runtime_error( "Cannot iterate with lengths < 2" ); //FIXME
+
+                // Corners in CCW order
+                callback( flatten( { 0, 0, 0 }, lengths ) );
+                callback( flatten( { lengths.at( 0 ) - 1, 0, 0 }, lengths ) );
+                callback( flatten( { lengths.at( 0 ) - 1, lengths.at( 1 ) - 1, 0 }, lengths ) );
+                callback( flatten( { 0, lengths.at( 1 ) - 1, 0 }, lengths ) );
+                callback( flatten( { 0, 0, lengths.at( 2 ) - 1 }, lengths ) );
+                callback( flatten( { lengths.at( 0 ) - 1, 0, lengths.at( 2 ) - 1 }, lengths ) );
+                callback( flatten( { lengths.at( 0 ) - 1, lengths.at( 1 ) - 1, lengths.at( 2 ) - 1 }, lengths ) );
+                callback( flatten( { 0, lengths.at( 1 ) - 1, lengths.at( 2 ) - 1 }, lengths ) );
+
+                // Edges of u = 0 face in CCW order, iterated in positive s or t direction
+                for( size_t i = 1; i < lengths.at( 0 ) - 1; i++ )
+                    callback( flatten( {i, 0, 0}, lengths ) );
+                for( size_t i = 1; i < lengths.at( 1 ) - 1; i++ )
+                    callback( flatten( {lengths.at( 0 ) - 1, i, 0}, lengths ) );
+                for( size_t i = 1; i < lengths.at( 0 ) - 1; i++ )
+                    callback( flatten( {i, lengths.at( 1 ) - 1, 0}, lengths ) );
+                for( size_t i = 1; i < lengths.at( 1 ) - 1; i++ )
+                    callback( flatten( {0, i, 0}, lengths ) );
+                // Edges of u = 1 face in CCW order, iterated in positive s or t direction
+                for( size_t i = 1; i < lengths.at( 0 ) - 1; i++ )
+                    callback( flatten( {i, 0, lengths.at( 2 ) - 1 }, lengths ) );
+                for( size_t i = 1; i < lengths.at( 1 ) - 1; i++ )
+                    callback( flatten( {lengths.at( 0 ) - 1, i, lengths.at( 2 ) - 1 }, lengths ) );
+                for( size_t i = 1; i < lengths.at( 0 ) - 1; i++ )
+                    callback( flatten( {i, lengths.at( 1 ) - 1, lengths.at( 2 ) - 1 }, lengths ) );
+                for( size_t i = 1; i < lengths.at( 1 ) - 1; i++ )
+                    callback( flatten( {0, i, lengths.at( 2 ) - 1 }, lengths ) );
+                //Edges with changing u coordinate in TP order, iterated in positive u direction
+                for( size_t i = 1; i < lengths.at( 2 ) - 1; i++ )
+                    callback( flatten( { 0, 0, i }, lengths ) );
+                for( size_t i = 1; i < lengths.at( 2 ) - 1; i++ )
+                    callback( flatten( { lengths.at( 0 ) - 1, 0, i }, lengths ) );
+                for( size_t i = 1; i < lengths.at( 2 ) - 1; i++ )
+                    callback( flatten( { 0, lengths.at( 1 ) - 1, i }, lengths ) );
+                for( size_t i = 1; i < lengths.at( 2 ) - 1; i++ )
+                    callback( flatten( { lengths.at( 0 ) - 1, lengths.at( 1 ) - 1, i }, lengths ) );
+
+                // s = 0 face
+                iterateTensorProduct( { lengths.at( 1 ) - 2, lengths.at( 2 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { 0, interior_index.at( 0 ) + 1, interior_index.at( 1 ) + 1 }, lengths ) );
+                } );
+                // s = 1 face
+                iterateTensorProduct( { lengths.at( 1 ) - 2, lengths.at( 2 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { lengths.at( 0 ) - 1, interior_index.at( 0 ) + 1, interior_index.at( 1 ) + 1 }, lengths ) );
+                } );
+                // t = 0 face
+                iterateTensorProduct( { lengths.at( 0 ) - 2, lengths.at( 2 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { interior_index.at( 0 ) + 1, 0, interior_index.at( 1 ) + 1 }, lengths ) );
+                } );
+                // t = 1 face
+                iterateTensorProduct( { lengths.at( 0 ) - 2,  lengths.at( 2 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { interior_index.at( 0 ) + 1, lengths.at( 1 ) - 1, interior_index.at( 1 ) + 1 }, lengths ) );
+                } );
+                // u = 0 face
+                iterateTensorProduct( { lengths.at( 0 ) - 2, lengths.at( 1 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { interior_index.at( 0 ) + 1, interior_index.at( 1 ) + 1, 0 }, lengths ) );
+                } );
+                // u = 1 face
+                iterateTensorProduct( { lengths.at( 0 ) - 2, lengths.at( 1 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { interior_index.at( 0 ) + 1, interior_index.at( 1 ) + 1, lengths.at( 2 ) - 1 }, lengths ) );
+                } );
+
+                // Interior in tensor product order
+                iterateTensorProduct( { lengths.at( 0 ) - 2, lengths.at( 1 ) - 2, lengths.at( 2 ) - 2 }, [&]( const IndexVec& interior_index ) {
+                    callback( flatten( { interior_index.at( 0 ) + 1, interior_index.at( 1 ) + 1, interior_index.at( 2 ) + 1 }, lengths ) );
+                } );
+            }
+            break;
+            default:
+            break;
+        }
+    }
+}
---
+++ src/util/IndexOperations.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <SmallVector.hpp>
+#include <functional>
+
+namespace util
+{
+    using IndexVec = SmallVector<size_t, 3>;
+    size_t flatten( const IndexVec& tp_indices, const IndexVec& lengths );
+
+    IndexVec unflatten( const size_t index, const IndexVec& lengths );
+
+    void iterateTensorProduct( const IndexVec& lengths, const std::function<void( const IndexVec& )>& callback );
+
+    void iterateVTKTPOrdering( const IndexVec& lengths, const std::function<void( const size_t& )>& callback );
+}
