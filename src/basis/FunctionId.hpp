455bc75508267c85ea6d1f4b22e1cbb308f74278
Caleb Goates <calebgoates@gmail.com>
2024-08-22 11:20:36 -0600

    add: ID base class that can be used for all the ids.

--- src/basis/FunctionId.hpp
+++ src/basis/FunctionId.hpp
@@ -1,38 +1,8 @@
 #pragma once
-#include <Eigen/Dense>
+#include <IdBase.hpp>
 
 namespace basis
 {
-    class FunctionId
-    {
-        public:
-        explicit FunctionId( const Eigen::Index id ) : mId( id ) {}
-        operator Eigen::Index() const { return mId; }
-
-        Eigen::Index id() const { return mId; }
-
-        bool operator<( const FunctionId& o ) const
-        {
-            return mId < o.id();
-        }
-
-        bool operator>( const FunctionId& o ) const
-        {
-            return mId > o.id();
-        }
-
-        bool operator==( const FunctionId& o ) const
-        {
-            return mId == o.id();
-        }
-
-        friend std::ostream& operator<<( std::ostream& os, const FunctionId& fid )
-        {
-            os << fid.mId;
-            return os;
-        }
-
-        private:
-        Eigen::Index mId;
-    };
+    struct FunctionIdTag;
+    using FunctionId = util::IdBase<FunctionIdTag>;
 }
---
+++ src/util/IdBase.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <Eigen/Dense>
+
+namespace util
+{
+    /// @brief An id class that provides type-safe ids.  See https://stackoverflow.com/a/63821945
+    /// @tparam T  A tag that differentiates ids from each other.
+    /// As a usage example,
+    /// struct FunctionIdTag;
+    /// using FunctionId = IdBase<FunctionIdTag>;
+    template<typename T>
+    class IdBase
+    {
+        public:
+        IdBase( const Eigen::Index id ) : mId( id ) {}
+        operator Eigen::Index() const { return mId; }
+
+        Eigen::Index id() const { return mId; }
+
+        bool operator<( const IdBase& o ) const
+        {
+            return mId < o.id();
+        }
+
+        bool operator>( const IdBase& o ) const
+        {
+            return mId > o.id();
+        }
+
+        friend std::ostream& operator<<( std::ostream& os, const IdBase& id )
+        {
+            os << id.mId;
+            return os;
+        }
+
+        private:
+        Eigen::Index mId;
+    };
+}
