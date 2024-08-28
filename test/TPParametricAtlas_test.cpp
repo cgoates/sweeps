f8e0c217b4a8411f50baa869955bbe58dd2d8049
Caleb Goates <calebgoates@gmail.com>
2024-08-28 16:54:04 -0600

    test: cornerCells of TPParametricAtlas

--- test/TPParametricAtlas_test.cpp
+++ test/TPParametricAtlas_test.cpp
@@ -43,3 +43,37 @@ TEST_CASE( "3x4 TP combinatorial map" )
     CHECK( util::equals( tp.parametricLengths( Cell( Dart( 37 ), 2 ) ), Eigen::Vector2d( 1.0, 0.4 ), 1e-10 ) );
     CHECK( util::equals( tp.parametricLengths( Cell( Dart( 30 ), 2 ) ), Eigen::Vector2d( 2.0, 0.3 ), 1e-10 ) );
 }
+
+TEST_CASE( "corners" )
+{
+    const auto source_topo = std::make_shared<const CombinatorialMap1d>( 3 );
+    const auto line_topo = std::make_shared<const CombinatorialMap1d>( 2 );
+    const auto tp_topo_2d = std::make_shared<const TPCombinatorialMap>( source_topo, line_topo );
+    const auto tp_topo_3d = std::make_shared<const TPCombinatorialMap>( tp_topo_2d, line_topo );
+
+    const auto source = std::make_shared<const ParametricAtlas1d>( source_topo, Eigen::Vector3d( 1, 2, 3 ) );
+    const auto line = std::make_shared<const ParametricAtlas1d>( line_topo, Eigen::Vector2d( 0.1, 0.2 ) );
+    const auto tp_2d = std::make_shared<const TPParametricAtlas>( tp_topo_2d, source, line );
+    const TPParametricAtlas tp_3d( tp_topo_3d, tp_2d, line );
+
+
+    const auto test_corners = []( const TPParametricAtlas& tp ) {
+        const size_t dim = tp.cmap().dim();
+        for( size_t i = 0; i < dim; i++ )
+        {
+            const auto corner_cells = cornerCells( tp, i );
+            SmallVector< BaryCoordIsZeroVec, 12 > vecs;
+            for( const auto& item : corner_cells )
+            {
+                const auto temp = parentDomainBoundary( tp, item );
+                CHECK( std::find( vecs.begin(), vecs.end(), temp ) );
+                vecs.push_back( temp );
+                CHECK( tp.cmap().dim() - i == std::reduce( vecs.back().begin(), vecs.back().end(), size_t{0}, []( const size_t accum, const bool b ){ return accum + size_t(b); } ) );
+            }
+            CHECK( vecs.size() == ( i == 0 ? 1 : dim ) * pow( 2, dim - i ) );
+        }
+    };
+
+    test_corners( *tp_2d );
+    test_corners( tp_3d );
+}
