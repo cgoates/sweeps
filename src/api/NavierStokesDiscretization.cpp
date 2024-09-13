8fc779a54119b811d3727c23760f88bc59451d05
Caleb Goates <calebgoates@gmail.com>
2024-09-13 16:44:32 -0600

    cleanup: Remove duplicate implementation of BSpline from knot vectors

--- src/api/NavierStokesDiscretization.cpp
+++ src/api/NavierStokesDiscretization.cpp
@@ -3,27 +3,6 @@
 
 namespace api
 {
-    basis::TPSplineSpace buildH1( const basis::KnotVector& kv_s,
-                                  const basis::KnotVector& kv_t,
-                                  const size_t degree_s,
-                                  const size_t degree_t )
-    {
-        const auto cmap_1d_s = std::make_shared<const topology::CombinatorialMap1d>( numElements( kv_s ) );
-        const auto cmap_1d_t = std::make_shared<const topology::CombinatorialMap1d>( numElements( kv_t ) );
-        const auto param_1d_s =
-            std::make_shared<const param::ParametricAtlas1d>( cmap_1d_s, parametricLengths( kv_s ) );
-        const auto param_1d_t =
-            std::make_shared<const param::ParametricAtlas1d>( cmap_1d_t, parametricLengths( kv_t ) );
-        const auto bc_1d_s = std::make_shared<const basis::BasisComplex1d>( param_1d_s, degree_s );
-        const auto bc_1d_t = std::make_shared<const basis::BasisComplex1d>( param_1d_t, degree_t );
-        const auto ss_1d_s = std::make_shared<const basis::BSplineSpace1d>( bc_1d_s, kv_s );
-        const auto ss_1d_t = std::make_shared<const basis::BSplineSpace1d>( bc_1d_t, kv_t );
-        const auto cmap_2d = std::make_shared<const topology::TPCombinatorialMap>( cmap_1d_s, cmap_1d_t );
-        const auto param_2d = std::make_shared<const param::TPParametricAtlas>( cmap_2d, param_1d_s, param_1d_t );
-        const auto H1_bc = std::make_shared<const basis::TPBasisComplex>( param_2d, bc_1d_s, bc_1d_t );
-        return basis::TPSplineSpace( H1_bc, ss_1d_s, ss_1d_t );
-    }
-
     basis::DivConfTPSplineSpace buildHDIV( const basis::TPSplineSpace& H1 )
     {
         const auto HDIV_bc = std::make_shared<basis::DivConfBasisComplex>( H1.basisComplexPtr() );
@@ -45,7 +24,7 @@ namespace api
                                                             const size_t degree_s,
                                                             const size_t degree_t,
                                                             const Eigen::Matrix2Xd& cpts )
-        : H1_ss( buildH1( kv_s, kv_t, degree_s, degree_t ) ),
+        : H1_ss( basis::buildBSpline( {kv_s, kv_t}, {degree_s, degree_t} ) ),
           HDIV_ss( buildHDIV( H1_ss ) ),
           L2_ss( buildL2( H1_ss.basisComplex().parametricAtlasPtr(), HDIV_ss ) ),
           cmap_bdry( H1_ss.basisComplex().parametricAtlas().cmap(), { topology::Dart( 0 ) } ),
