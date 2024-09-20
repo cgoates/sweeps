49f1109a9380f98151bdf91bbf6b10836391bcbf
Caleb Goates <calebgoates@gmail.com>
2024-09-20 09:53:57 -0600

    test: Test triadic refinement on cmap and spline space

--- test/HierarchicalTPCMap_test.cpp
+++ test/HierarchicalTPCMap_test.cpp
@@ -10,21 +10,19 @@ TEST_CASE( "Simple hierarchical cmap 1" )
 {
     const auto topo1d_1 = std::make_shared<const CombinatorialMap1d>( 1 );
     const auto topo1d_2 = std::make_shared<const CombinatorialMap1d>( 2 );
-    const auto topo1d_4 = std::make_shared<const CombinatorialMap1d>( 4 );
-    const auto topo1d_8 = std::make_shared<const CombinatorialMap1d>( 8 );
+    const auto topo1d_3 = std::make_shared<const CombinatorialMap1d>( 3 );
+    const auto topo1d_6 = std::make_shared<const CombinatorialMap1d>( 6 );
     const auto tp_topo_1 = std::make_shared<const TPCombinatorialMap>( topo1d_1, topo1d_2 );
-    const auto tp_topo_2 = std::make_shared<const TPCombinatorialMap>( topo1d_2, topo1d_4 );
-    const auto tp_topo_3 = std::make_shared<const TPCombinatorialMap>( topo1d_4, topo1d_8 );
+    const auto tp_topo_2 = std::make_shared<const TPCombinatorialMap>( topo1d_3, topo1d_6 );
 
-    const HierarchicalTPCombinatorialMap cmap( { tp_topo_1, tp_topo_2, tp_topo_3 }, {
+    const HierarchicalTPCombinatorialMap cmap( { tp_topo_1, tp_topo_2 }, {
         { Face( Dart( 4 ) ) },
-        { Face( Dart( 0 ) ), Face( Dart( 4 ) ), Face( Dart( 8 ) ), Face( Dart( 12 ) ) },
-        {}
+        { Face( Dart( 0 ) ), Face( Dart( 4 ) ), Face( Dart( 8 ) ), Face( Dart( 12 ) ), Face( 16 ), Face( 20 ), Face( 24 ), Face( 28 ), Face( 32 ) }
     } );
 
-    CHECK( cellCount( cmap, 2 ) == 5 );
-    CHECK( cellCount( cmap, 1 ) == 15 );
-    CHECK( cellCount( cmap, 0 ) == 11 );
+    CHECK( cellCount( cmap, 2 ) == 10 );
+    CHECK( cellCount( cmap, 1 ) == 27 );
+    CHECK( cellCount( cmap, 0 ) == 18 );
 
     size_t n_darts = 0;
     iterateDartsWhile( cmap, [&]( const Dart& d ){
@@ -38,12 +36,13 @@ TEST_CASE( "Simple hierarchical cmap 1" )
         return true;
     } );
 
-    CHECK( n_darts == 21 );
+    CHECK( n_darts == 42 );
 
     CHECK( cmap.unrefinedAncestorDart( Dart( 22 ) ) == cmap.dartRanges().toLocalDart( Dart( 22 ) ) );
     CHECK( cmap.unrefinedAncestorDart( Dart( 5 ) ) == cmap.dartRanges().toLocalDart( Dart( 5 ) ) );
-    CHECK( cmap.unrefinedAncestorDart( Dart( 24 ) ).second.id() == 4 );
-    CHECK( cmap.unrefinedAncestorDart( Dart( 28 ) ).second.id() == 4 );
+    CHECK( cmap.unrefinedAncestorDart( Dart( 44 ) ).second.id() == 4 );
+    CHECK( cmap.unrefinedAncestorDart( Dart( 48 ) ).second.id() == 4 );
+    CHECK( cmap.unrefinedAncestorDart( Dart( 52 ) ).second.id() == 4 );
 }
 
 TEST_CASE( "Simple hierarchical cmap 2" )
--- test/HierarchicalTPSplineSpace_test.cpp
+++ test/HierarchicalTPSplineSpace_test.cpp
@@ -118,3 +118,64 @@ TEST_CASE( "Simple hierarchical atlas 2" )
     test_center_pt( Face( 12 ), Eigen::Vector3d( 0.75, 0.25, 0.0 ) );
     test_center_pt( Face( 80 ), Eigen::Vector3d( 0.625, 0.625, 0.0 ) );
 }
+
+TEST_CASE( "Simple hierarchical atlas 3" )
+{
+    const KnotVector kv1( { 0, 0, 1, 1}, 1e-10 );
+    const KnotVector kv2( { 0, 0, 1, 2, 2}, 1e-10 );
+    const KnotVector kv3 = nAdicRefine( kv1, 3 );
+    const KnotVector kv6 = nAdicRefine( kv2, 3 );
+
+    const size_t degree = 1;
+    const auto tp_ss_1 = std::make_shared<const TPSplineSpace>( buildBSpline( { kv1, kv2 }, { degree, degree } ) );
+    const auto tp_ss_2 = std::make_shared<const TPSplineSpace>( buildBSpline( { kv3, kv6 }, { degree, degree } ) );
+
+    const HierarchicalTPSplineSpace ss = buildHierarchicalSplineSpace( { tp_ss_1, tp_ss_2 }, {
+        { Face( Dart( 4 ) ) },
+        { Face( Dart( 0 ) ), Face( Dart( 4 ) ), Face( Dart( 8 ) ), Face( Dart( 12 ) ), Face( 16 ), Face( 20 ), Face( 24 ), Face( 28 ), Face( 32 ) }
+    } );
+
+    const Eigen::MatrixX3d geom = ( Eigen::MatrixX3d( 16, 3 ) <<
+        0, 1, 0,
+        1, 1, 0,
+        0, 2, 0,
+        1, 2, 0,
+        0, 0, 0,
+        0.3333, 0, 0,
+        0.6666, 0, 0,
+        1, 0, 0,
+        0, 0.3333, 0,
+        0.3333, 0.3333, 0,
+        0.6666, 0.3333, 0,
+        1, 0.3333, 0,
+        0, 0.6666, 0,
+        0.3333, 0.6666, 0,
+        0.6666, 0.6666, 0,
+        1, 0.6666, 0 ).finished();
+
+    if( VTK_OUTPUT ) io::outputBezierMeshToVTK( ss, geom, "hier_bez_test.vtu" );
+
+    eval::SplineSpaceEvaluator evaler( ss, 0 );
+
+    const auto& atlas = ss.basisComplex().parametricAtlas();
+    const auto test_center_pt = [&]( const Face& f, const Eigen::Vector3d& expected ) {
+        const ParentPoint ppt = pointOnBoundary( atlas.parentDomain( f ), parentDomainBoundary( atlas, f ) );
+        evaler.localizeElement( f );
+        evaler.localizePoint( ppt );
+
+        const Eigen::Vector3d pt = evaler.evaluateManifold( geom.transpose() );
+        std::cout << pt.transpose() << std::endl;
+
+        CHECK( util::equals( pt, expected, 1e-4 ) );
+    };
+
+    test_center_pt( Face( 5 ), Eigen::Vector3d( 0.5, 1.5, 0.0 ) );
+    test_center_pt( Face( 8 ), Eigen::Vector3d( 1.0/6.0, 1.0/6.0, 0.0 ) );
+    test_center_pt( Face( 12 ), Eigen::Vector3d( 0.5, 1.0/6.0, 0.0 ) );
+    test_center_pt( Face( 32 ), Eigen::Vector3d( 1.0/6.0, 5.0/6.0, 0.0 ) );
+
+    iterateCellsWhile( ss.basisComplex().parametricAtlas().cmap(), 2, [&]( const Face f ){
+        std::cout << f << std::endl << ss.connectivity( f ) << std::endl << ss.extractionOperator( f ) << std::endl << std::endl;
+        return true;
+    } );
+}
