365f25c156e22d00c68d99b9415318f12a5beb2d
Caleb Goates <calebgoates@gmail.com>
2025-01-21 17:49:55 -0700

    test: Simple test of 3d hierarchical spline space

--- test/HierarchicalTPSplineSpace_test.cpp
+++ test/HierarchicalTPSplineSpace_test.cpp
@@ -179,3 +179,58 @@ TEST_CASE( "Simple hierarchical atlas 3" )
         return true;
     } );
 }
+
+TEST_CASE( "Simple 3d hierarchical atlas 1" )
+{
+    const KnotVector kv1( {0, 0, 0, 1, 1, 1}, 1e-10 );
+    const KnotVector kv2( {0, 0, 0, 0.5, 1, 1, 1}, 1e-10 );
+    const KnotVector kv4( {0, 0, 0, 0.25, 0.5, 0.75, 1, 1, 1}, 1e-10 );
+
+    const size_t degree = 2;
+    const auto tp_ss_1 = std::make_shared<const TPSplineSpace>( buildBSpline( { kv1, kv1, kv2 }, { degree, degree, degree } ) );
+    const auto tp_ss_2 = std::make_shared<const TPSplineSpace>( buildBSpline( { kv2, kv2, kv4 }, { degree, degree, degree } ) );
+
+    const HierarchicalTPSplineSpace ss = buildHierarchicalSplineSpace( { tp_ss_1, tp_ss_2 }, {
+        { Volume( 24 ) },
+        { Volume( 0 ), Volume( 24 ), Volume( 48 ), Volume( 72 ), Volume( 96 ), Volume( 120 ), Volume( 144 ), Volume( 168 ) }
+    } );
+
+    Eigen::MatrixX3d geom( ss.numFunctions(), 3 );
+    const std::vector<Eigen::MatrixXd> geoms = { util::tensorProduct( { grevillePoints( kv1, degree ), grevillePoints( kv1, degree ), grevillePoints( kv2, degree ) } ),
+                                                 util::tensorProduct( { grevillePoints( kv2, degree ), grevillePoints( kv2, degree ), grevillePoints( kv4, degree ) } ) };
+    size_t ii = 0;
+    size_t j = 0;
+    for( const auto& active_funcs : ss.activeFunctions() )
+    {
+        for( const auto& f : active_funcs )
+        {
+            geom.row( ii++ ) = geoms.at( j ).row( f );
+        }
+        j++;
+    }
+
+    if( VTK_OUTPUT ) io::outputBezierMeshToVTK( ss, geom, "hier_bez_test.vtu" );
+
+    eval::SplineSpaceEvaluator evaler( ss, 0 );
+
+    const auto& atlas = ss.basisComplex().parametricAtlas();
+    const auto test_center_pt = [&]( const Volume& v, const Eigen::Vector3d& expected ) {
+        const ParentPoint ppt = pointOnBoundary( atlas.parentDomain( v ), parentDomainBoundary( atlas, v ) );
+        evaler.localizeElement( v );
+        evaler.localizePoint( ppt );
+
+        const Eigen::Vector3d pt = evaler.evaluateManifold( geom.transpose() );
+
+        CHECK( util::equals( pt, expected, 1e-9 ) );
+    };
+
+    test_center_pt( Volume( 26 ), Eigen::Vector3d( 0.5, 0.5, 0.75 ) );
+    test_center_pt( Volume( 48 ), Eigen::Vector3d( 0.25, 0.25, 0.125 ) );
+    test_center_pt( Volume( 72 ), Eigen::Vector3d( 0.75, 0.25, 0.125 ) );
+    test_center_pt( Volume( 96 ), Eigen::Vector3d( 0.25, 0.75, 0.125 ) );
+    test_center_pt( Volume( 120 ), Eigen::Vector3d( 0.75, 0.75, 0.125 ) );
+    test_center_pt( Volume( 144 ), Eigen::Vector3d( 0.25, 0.25, 0.375 ) );
+    test_center_pt( Volume( 168 ), Eigen::Vector3d( 0.75, 0.25, 0.375 ) );
+    test_center_pt( Volume( 192 ), Eigen::Vector3d( 0.25, 0.75, 0.375 ) );
+    test_center_pt( Volume( 216 ), Eigen::Vector3d( 0.75, 0.75, 0.375 ) );
+}
