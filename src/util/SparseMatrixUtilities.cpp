1e6e52670a7107fb314bb1467ba16bc6482ffeb1
Caleb Goates <calebgoates@gmail.com>
2025-05-05 14:34:54 -0600

    add: Vertical concatenator for sparse matrices into a premade matrix.

--- src/util/SparseMatrixUtilities.cpp
+++ src/util/SparseMatrixUtilities.cpp
@@ -3,9 +3,20 @@
 
 namespace util
 {
+    using namespace Eigen;
+
     // See https://stackoverflow.com/a/41777589
     Eigen::SparseMatrix<double> verticalConcat( const Eigen::SparseMatrix<double>& mat1,
                                                 const Eigen::SparseMatrix<double>& mat2 )
+    {
+        SparseMatrix<double> M( mat1.rows() + mat2.rows(), mat1.cols() );
+        verticalConcatInto( mat1, mat2, M );
+        return M;
+    }
+
+    void verticalConcatInto( const Eigen::SparseMatrix<double>& mat1,
+                             const Eigen::SparseMatrix<double>& mat2,
+                             Eigen::SparseMatrix<double>& out )
     {
         if( mat1.cols() != mat2.cols() )
         {
@@ -13,19 +24,22 @@ namespace util
             std::cerr << "Matrix 2 has size " << mat2.rows() << ", " << mat2.cols() << ".\n";
             throw std::invalid_argument( "Input matrices must have the same number of columns." );
         }
-        using namespace Eigen;
-        SparseMatrix<double> M( mat1.rows() + mat2.rows(), mat1.cols() );
-        M.reserve( mat1.nonZeros() + mat2.nonZeros() );
+        if( out.cols() != mat1.cols() or out.rows() != mat1.rows() + mat2.rows() or out.nonZeros() != 0 )
+        {
+            out = SparseMatrix<double>( mat1.rows() + mat2.rows(), mat1.cols() );
+        }
+
+
+        out.reserve( mat1.nonZeros() + mat2.nonZeros() );
         for( Index c = 0; c < mat1.cols(); ++c )
         {
-            M.startVec( c ); // Important: Must be called once for each column before inserting!
+            out.startVec( c ); // Important: Must be called once for each column before inserting!
             for( SparseMatrix<double>::InnerIterator itL( mat1, c ); itL; ++itL )
-                M.insertBack( itL.row(), c ) = itL.value();
+                out.insertBack( itL.row(), c ) = itL.value();
             for( SparseMatrix<double>::InnerIterator itC( mat2, c ); itC; ++itC )
-                M.insertBack( itC.row() + mat1.rows(), c ) = itC.value();
+                out.insertBack( itC.row() + mat1.rows(), c ) = itC.value();
         }
-        M.finalize();
-        return M;
+        out.finalize();
     }
 
     Eigen::SparseMatrix<double> sparseIdentity( const Eigen::Index n )
--- src/util/SparseMatrixUtilities.hpp
+++ src/util/SparseMatrixUtilities.hpp
@@ -6,5 +6,11 @@ namespace util
     Eigen::SparseMatrix<double> verticalConcat( const Eigen::SparseMatrix<double>& mat1,
                                                 const Eigen::SparseMatrix<double>& mat2 );
 
+    // Concatenates two matrices vertically into a provided output matrix.
+    // Can be more performant than the version that returns a matrix in some scenarios.
+    void verticalConcatInto( const Eigen::SparseMatrix<double>& mat1,
+                             const Eigen::SparseMatrix<double>& mat2,
+                             Eigen::SparseMatrix<double>& out );
+
     Eigen::SparseMatrix<double> sparseIdentity( const Eigen::Index n );
 }
