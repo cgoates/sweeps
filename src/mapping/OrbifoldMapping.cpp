9529e25baf99bf391a866e6a163e5f709660d3ba
Caleb Goates <calebgoates@gmail.com>
2025-06-02 17:06:12 -0600

    fix: Fix issues with tutte orbifold embedding

--- src/mapping/OrbifoldMapping.cpp
+++ src/mapping/OrbifoldMapping.cpp
@@ -36,9 +36,9 @@ std::pair<Eigen::Vector2i, RotationType> getTransform( const Eigen::Vector2d& pt
 
     if( int_part( 0 ) % 2 != 0 and int_part( 1 ) % 2 != 0 )
         return { int_part, RotationType::OneEighty };
-    else if( int_part( 0 ) % 2 != 0 )
-        return { int_part, RotationType::CounterClockwise };
     else if( int_part( 1 ) % 2 != 0 )
+        return { int_part, RotationType::CounterClockwise };
+    else if( int_part( 0 ) % 2 != 0 )
         return { int_part, RotationType::Clockwise };
     else
         return { int_part, RotationType::None };
@@ -62,17 +62,8 @@ OrbifoldMapping::OrbifoldMapping( const std::shared_ptr<const param::TrianglePar
         // If the triangle max is greater than 1 in either dimension, or min is less than 0 in either dimension,
         // we need to create copies of it. If the whole triangle is outside the canonical domain, we can just use
         // the rotated copy.
-        if( ( aabb.min().array() > 1.0 ).any() or ( aabb.max().array() < 0.0 ).any() )
-        {
-            const Triangle<2> tri_copy{ transformToCanonicalDomain( tri.v1 ),
-                                        transformToCanonicalDomain( tri.v2 ),
-                                        transformToCanonicalDomain( tri.v3 ) };
-            const AABB aabb_copy = aabbFromTriangle( tri_copy );
-            mTriangles.push_back( OrbifoldTriangle{ f, { std::pair<AABB, Triangle<2>>( aabb_copy, tri_copy ) } } );
-        }
-        else if( ( aabb.max().array() > 1.0 ).any() or ( aabb.min().array() < 0.0 ).any() )
-        {
-            const auto [translation, rotation] = getTransform( tri.v1 );
+        const auto add_with_rotation = [&]( const Eigen::Vector2i& translation,
+                                            const RotationType& rotation ) {
             const Triangle<2> tri_copy{ transform( tri.v1, translation, rotation ),
                                         transform( tri.v2, translation, rotation ),
                                         transform( tri.v3, translation, rotation ) };
@@ -80,6 +71,36 @@ OrbifoldMapping::OrbifoldMapping( const std::shared_ptr<const param::TrianglePar
             mTriangles.emplace_back( OrbifoldTriangle{
                 f,
                 { std::pair<AABB, Triangle<2>>( aabb, tri ), std::pair<AABB, Triangle<2>>( aabb_copy, tri_copy ) } } );
+        };
+        if( ( aabb.min().array() > 1.0 ).any() or ( aabb.max().array() < 0.0 ).any() )
+        {
+            // Do we need to check the 180 degrees thing here as well?
+            const Triangle<2> tri_copy{ transformToCanonicalDomain( tri.v1 ),
+                                        transformToCanonicalDomain( tri.v2 ),
+                                        transformToCanonicalDomain( tri.v3 ) };
+            const AABB aabb_copy = aabbFromTriangle( tri_copy );
+            mTriangles.push_back( OrbifoldTriangle{ f, { std::pair<AABB, Triangle<2>>( aabb_copy, tri_copy ) } } );
+        }
+        else if( ( aabb.max().array() > 1.0 ).any() )
+        {
+            const auto [translation, rotation] = [&]() -> std::pair<Eigen::Vector2i, RotationType> {
+                const auto [translation, rotation] = getTransform( aabb.max() );
+                if( rotation == RotationType::OneEighty )
+                {
+                    Eigen::Index max_index;
+                    const double max_val = aabb.max().maxCoeff( &max_index );
+                    const Eigen::Vector2d point = tri.v1( max_index ) == max_val ? tri.v1 : ( tri.v2( max_index ) == max_val ? tri.v2 : tri.v3 );
+                    return getTransform( point );
+                }
+                else return { translation, rotation };
+            }();
+            add_with_rotation( translation, rotation );
+        }
+        else if( ( aabb.min().array() < 0.0 ).any() )
+        {
+            // Do we need to check the 180 degrees thing here as well?
+            const auto [translation, rotation] = getTransform( aabb.min() );
+            add_with_rotation( translation, rotation );
         }
         else
         {
@@ -125,7 +146,8 @@ std::optional<std::pair<topology::Cell, param::ParentPoint>> OrbifoldMapping::ma
             }
         }
     }
-    throw std::runtime_error( "No inverse found on plane-tiling orbifold" );
+    throw std::runtime_error( "No inverse found on plane-tiling orbifold " + std::to_string( pt( 0 ) ) + " " +
+                              std::to_string( pt( 1 ) ) );
 }
 
 std::pair<topology::Cell, param::ParentPoint> OrbifoldMapping::closestPoint( const Vector3dMax& pt ) const
