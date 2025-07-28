647e53b0f9eb09499491c00a6f37c80deb31d355
Caleb Goates <calebgoates@gmail.com>
2025-07-28 15:23:38 -0600

    add: Function which allows fitting of a spline space to a manifold on the same parametric atlas

--- src/fitting/LeastSquaresFitting.cpp
+++ src/fitting/LeastSquaresFitting.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <Eigen/Sparse>
 #include <Eigen/IterativeLinearSolvers>
+#include <MultiPatchSplineSpace.hpp>
+#include <TPSplineSpace.hpp>
+#include <set>
+#include <SimplicialComplex.hpp>
 
 namespace fitting
 {
@@ -37,5 +41,96 @@ namespace fitting
         Eigen::LeastSquaresConjugateGradient<Eigen::SparseMatrix<double>> solver;
         solver.compute( A );
         return solver.solve( rhs );
+    }
+
+    Eigen::MatrixXd fitToManifold( const basis::MultiPatchSplineSpace& manifold_ss,
+                                   const Eigen::MatrixXd& manifold_cpts,
+                                   const basis::MultiPatchSplineSpace& new_ss )
+    {
+        if( &manifold_ss.basisComplex().parametricAtlas() != &new_ss.basisComplex().parametricAtlas() )
+        {
+            throw std::runtime_error( "Manifold and new spline space must have the same parametric domain." );
+        }
+
+        const Eigen::MatrixXd manifold_cpts_transposed = manifold_cpts.transpose(); // FIXME
+
+        // Get the greville points in the parent domain
+        std::vector<std::vector<std::pair<topology::Cell, param::ParentPoint>>> greville_ppts;
+        greville_ppts.reserve( new_ss.subSpaces().size() );
+
+        for( size_t patch_ii = 0; patch_ii < new_ss.subSpaces().size(); patch_ii++ )
+        {
+            const auto& subspace = new_ss.subSpaces().at( patch_ii );
+            const basis::TPSplineSpace& tp_subspace = static_cast<const basis::TPSplineSpace&>( *subspace );
+            const topology::TPCombinatorialMap& source_cmap = static_cast<const topology::TPCombinatorialMap&>(
+                tp_subspace.source().basisComplex().parametricAtlas().cmap() );
+            const auto ss_1ds = tensorProductComponentSplines( tp_subspace );
+
+            auto& patch_greville_ppts = greville_ppts.emplace_back();
+
+            SmallVector<std::vector<std::pair<topology::Cell, param::ParentPoint>>, 3> ppts;
+            for( const auto& component : ss_1ds )
+            {
+                const Eigen::VectorXd greville_pts = grevillePoints(
+                    component->knotVector(), degrees( component->basisComplex().defaultParentBasis() ).at( 0 ) );
+                const std::vector<double> greville_vec( greville_pts.data(), greville_pts.data() + greville_pts.size() );
+                ppts.push_back(
+                    parentPointsOfParamPoints( greville_vec, component->basisComplex().parametricAtlas(), 1e-9 ) );
+            }
+
+            for( const auto& pt3 : ppts.at( 2 ) )
+            {
+                for( const auto& pt2 : ppts.at( 1 ) )
+                {
+                    for( const auto& pt1 : ppts.at( 0 ) )
+                    {
+                        const topology::Volume patch_cell( tp_subspace.basisComplex().parametricAtlas().cmap().flatten(
+                            source_cmap.flatten(
+                                pt1.first.dart(), pt2.first.dart(), topology::TPCombinatorialMap::TPDartPos::DartPos0 ),
+                            pt3.first.dart(),
+                            topology::TPCombinatorialMap::TPDartPos::DartPos0 ) );
+                        const topology::Volume cell( new_ss.basisComplex().parametricAtlas().cmap().toGlobalDart(
+                            patch_ii, patch_cell.dart() ) );
+                        patch_greville_ppts.emplace_back( std::make_pair(
+                            cell,
+                            param::tensorProduct( param::tensorProduct( pt1.second, pt2.second ), pt3.second ) ) );
+                    }
+                }
+            }
+        }
+
+        eval::SplineSpaceEvaluator new_evaler( new_ss, 0 );
+        eval::SplineSpaceEvaluator manifold_evaler( manifold_ss, 0 );
+        return leastSquaresFitting(
+            new_evaler,
+            new_ss.numFunctions(),
+            manifold_cpts.cols(),
+            [&]( const std::function<void( const topology::Cell&, const param::ParentPoint&, const Eigen::VectorXd& )>&
+                     add_least_squares_point ) {
+                const auto& mp_fid_map = new_ss.functionIdMap();
+                std::set<basis::FunctionId> seen_fids;
+                for( size_t patch_ii = 0; patch_ii < mp_fid_map.size(); patch_ii++ )
+                {
+                    for( size_t patch_fid = 0; patch_fid < mp_fid_map.at( patch_ii ).size(); patch_fid++ )
+                    {
+                        const basis::FunctionId fid = mp_fid_map.at( patch_ii ).at( patch_fid );
+                        if( seen_fids.count( fid ) > 0 ) continue;
+                        seen_fids.insert( fid );
+
+                        const auto& cell_pt = greville_ppts.at( patch_ii ).at( patch_fid );
+
+                        const topology::Cell& cell = cell_pt.first;
+                        const param::ParentPoint& ppt = cell_pt.second;
+
+                        manifold_evaler.localizeElement( cell );
+                        manifold_evaler.localizePoint( ppt );
+
+                        const Eigen::VectorXd manifold_pt =
+                            manifold_evaler.evaluateManifold( manifold_cpts_transposed );
+
+                        add_least_squares_point( cell, ppt, manifold_pt );
+                    }
+                }
+            } );
     }
 }
--- src/fitting/LeastSquaresFitting.hpp
+++ src/fitting/LeastSquaresFitting.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <SplineSpaceEvaluator.hpp>
 
+namespace basis
+{
+    class MultiPatchSplineSpace;
+}
+
 namespace fitting
 {
     Eigen::MatrixXd leastSquaresFitting(
@@ -8,4 +13,13 @@ namespace fitting
         const size_t n_points,
         const size_t spatial_dim,
         const std::function<void( const std::function<void(const topology::Cell&, const param::ParentPoint&, const Eigen::VectorXd&)>& )>& point_iterator );
+
+    /// @brief Fits a spline space to a manifold defined over the same parametric atlas.
+    /// @param manifold_ss   The spline space defining the manifold.
+    /// @param manifold_cpts The control points defining the manifold.
+    /// @param new_ss        The spline space to fit to the manifold.
+    /// @return The control points of the new spline space fitted to the manifold.
+    Eigen::MatrixXd fitToManifold( const basis::MultiPatchSplineSpace& manifold_ss,
+                                   const Eigen::MatrixXd& manifold_cpts,
+                                   const basis::MultiPatchSplineSpace& new_ss );
 }
