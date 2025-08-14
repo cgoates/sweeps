c28a7513a5d438cbf4e95d03e59378f6983aab50
Colby Johnson <colbyj427>
2025-08-14 09:25:25 -0600

    created trivial pytest file and added to CMake, verified it runs.

---
+++ test/test_QuadMesh.py
@@ -0,0 +1,10 @@
+# import sweeps
+
+
+def test_QuadMesh():
+    #Get the test file, hookBase.vtk maybe.
+    #Create a quadMesh object
+    #Make sure it worked by checking lengths.
+    #Add other assertions where necessary.
+    check = True
+    assert check == True
